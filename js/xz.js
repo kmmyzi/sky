20042c9920d45688d79fd32c9d041eacb3284c67
guojiawei <18734869369@163.com>
2017-05-22 10:25:43 +0800

    首页完成

---
+++ js/xz.js
@@ -0,0 +1,227 @@
+$(function(){
+	$(".more").hover(function(){
+	    $(this).animate({         
+    width:"180px",
+    background:"#febe05"
+  }, 400 ); 
+	},function(){
+	    $(this).animate({         
+    width:"150px",
+    background:"transparent"
+  }, 400 );
+	});
+	$(".specialtyConContent").hover(function(){
+	    $(this).children(".dinwei").children(".title").animate({         
+   color: "#f9c34a",
+   marginLeft: "30px"
+}, 400 );
+	},function(){
+	   $(this).children(".dinwei").children(".title").animate({
+    color:"#434343",
+    marginLeft: "0px"
+}, 400 );
+})
+	$(".specialtyConContent").hover(function(){
+	    $(this).children(".dinwei").children(".content").animate({         
+   marginLeft: "30px"
+}, 400 );
+	},function(){
+	   $(this).children(".dinwei").children(".content").animate({
+    
+    marginLeft: "0px"
+}, 400 );
+})  
+	$(".specialtyConContent").hover(function(){
+	    $(this).children(".math").animate({         
+    fontSize: "70px", 
+    fontWeight: "900", 
+    marginTop: "100px"
+  }, 400 );
+	},function(){
+	   $(this).children(".math").animate({
+    fontSize: "36px", 
+    fontWeight: "400", 
+    marginTop: "129px"
+  }, 400 );
+	});
+	$(".specialtyConContent").hover(function(){
+	    $(this).children(".gang").animate({         
+    top: "150px", 
+    left:"76px",
+    width: "56px",
+    height:"1px",
+    backgroundColor: "#f9c34a"
+}, 400 );
+	},function(){
+	   $(this).children(".gang").animate({
+    top: "150px", 
+    left: "36px", 
+    width:"36px",
+    height: "1px",
+    backgroundColor:"#434343"
+}, 400 );
+	});
+	$(".specialtyConContent").hover(function(){
+	    $(this).children(".ckxq").animate({         
+    marginLeft: "-60px", 
+    width:"120px"
+    
+}, 400 );
+	},function(){
+	   $(this).children(".ckxq").animate({
+    marginLeft: "-55px", 
+    width:"110px"
+}, 400 );
+	});
+	$(".img").hover(function(){
+	    $(this).children(".ts").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".ts").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".img").hover(function(){
+	    $(this).children(".tx").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".tx").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".img").hover(function(){
+	    $(this).children(".tz").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".tz").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".img").hover(function(){
+	    $(this).children(".ty").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".ty").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	
+	$(".img").hover(function(){
+	    $(this).children(".lian").children("img").animate({         
+    borderWidth: "10px"
+  }, 400 );
+	},function(){
+	   $(this).children(".lian").children("img").animate({
+    borderWidth: "0px"
+  }, 400 );
+	});
+	$(".img").hover(function(){
+	    $(this).children(".lian").animate({         
+   marginLeft: "-34.5px",
+   marginTop:"-34.5px"
+  }, 400 );
+	},function(){
+	   $(this).children(".lian").animate({
+    marginLeft: "-24.5px",
+   marginTop:"-24.5px"
+  }, 400 );
+	});
+//rwtx
+$(".rwtx").hover(function(){
+	    $(this).children(".al").animate({         
+    borderWidth: "10px",
+    marginLeft: "-38.5px",
+   marginTop:"-38.5px"
+  }, 400 );
+	},function(){
+	   $(this).children(".al").animate({
+    borderWidth: "0px",
+    marginLeft: "-28.5px",
+   marginTop:"-28.5px"
+  }, 400 );
+	});
+	$(".rwtx").hover(function(){
+	    $(this).children(".rs").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".rs").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".rwtx").hover(function(){
+	    $(this).children(".ry").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".ry").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".rwtx").hover(function(){
+	    $(this).children(".rx").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".rx").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".rwtx").hover(function(){
+	    $(this).children(".rz").animate({         
+    opacity: "1"
+  }, 400 );
+	},function(){
+	   $(this).children(".rz").animate({
+    opacity: "0"
+  }, 400 );
+	});
+	$(".xy a").hover(function(){
+	    $(this).children(".xyImg").children("img").animate({         
+    borderWidth: "10px",
+    marginLeft: "41px",
+   marginTop:"65px",
+   marginBottom:"0px"
+  }, 400 );
+	},function(){
+	   $(this).children(".xyImg").children("img").animate({
+    borderWidth: "1px",
+    marginLeft: "46px",
+   marginTop:"70px",
+   marginBottom:"10px"
+  }, 400 );
+	});
+	$(".trdtFloat").hover(function(){
+	    $(this).children(".trdtTitle").animate({         
+    left: "160px"
+  }, 400 );
+	},function(){
+	   $(this).children(".trdtTitle").animate({
+    left: "134px"
+  }, 400 );
+	});
+	$(".trdtFloat").hover(function(){
+	    $(this).children(".trdtBd").children(".s").animate({         
+    color: "#FEBE05"
+  }, 400 );
+	},function(){
+	   $(this).children(".trdtBd").children(".s").animate({
+    color: "#434343"
+  }, 400 );
+	});
+	$(".footerConRightConA").hover(function(){
+	    $(this).children("a").animate({         
+    color:"#fff"
+  }, 400 );
+	},function(){
+	   $(this).children("a").animate({
+    color:"#838383"
+  }, 400 );
+	});
+//	linear-gradient(to right, #16191c 15%, #eee 50%, #16191c 85%)
+})
+//	footerConRightConA
